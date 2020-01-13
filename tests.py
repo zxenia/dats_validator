8d4ede5a816b943b2e8032465b8647d07a63569a
zxenia <ksenia1zaytseva@gmail.com>
2020-01-13 12:01:21 -0500

    minor changes

--- tests.py
+++ tests.py
@@ -1,7 +1,8 @@
 import unittest
-from validator import validate_dats
+from validator import validate_json
 import json
 import os
+
 
 EXAMPLES = os.path.dirname(os.path.realpath(__file__)) + '/examples/'
 
@@ -9,15 +10,16 @@ class JsonschemaTest(unittest.TestCase):
     valid =  os.path.join(EXAMPLES, 'valid_dats.json')
     invalid = os.path.join(EXAMPLES, 'invalid_dats.json')
 
-    def test_validate_dats(self):
-        with open(self.valid) as vjson:
-            valid_obj = json.load(vjson)
-            valid_validation = validate_dats(valid_obj)
-        with open(self.invalid) as invjson:
-            invalid_obj = json.load(invjson)
-            invalid_validation = validate_dats(invalid_obj)
+    def test_validate_json(self):
+        with open(self.valid) as v_file:
+            valid_obj = json.load(v_file)
+            valid_validation = validate_json(valid_obj)
+        with open(self.invalid) as inv_file:
+            invalid_obj = json.load(inv_file)
+            invalid_validation = validate_json(invalid_obj)
         self.assertEqual(valid_validation, True)
         self.assertEqual(invalid_validation, False)
+
 
 if __name__ == '__main__':
     unittest.main()
--- validator.py
+++ validator.py
@@ -7,18 +7,18 @@ from sys import argv
 
 
 logger = logging.getLogger(__name__)
-DATS_PATH = os.path.dirname(os.path.realpath(__file__)) + '/dats/dataset_schema.json'
-
+# path to a top-level schema
+SCHEMA_PATH = os.path.dirname(os.path.realpath(__file__)) + '/dats/dataset_schema.json'
 
 def main(argv):
     FORMAT = '%(message)s'
     logging.basicConfig(format=FORMAT)
     logging.getLogger().setLevel(logging.INFO)
-    opts, args = getopt.getopt(argv, "", ["json="])
+    opts, args = getopt.getopt(argv, "", ["file="])
     json_filename = ''
 
     for opt, arg in opts:
-        if opt == '--json':
+        if opt == '--file':
             json_filename = arg
 
     if json_filename == '':
@@ -27,22 +27,22 @@ def main(argv):
 
     with open(json_filename) as json_file:
         json_obj = json.load(json_file)
-        validate_dats(json_obj)
+        validate_json(json_obj)
 
 
-def validate_dats(dats_json):
-    with open(DATS_PATH) as s:
+def validate_json(json_obj):
+    with open(SCHEMA_PATH) as s:
         json_schema = json.load(s)
     # first validate schema file
     v = jsonschema.Draft4Validator(json_schema)
-    # now validate dats file
+    # now validate json file
     try:
-        jsonschema.validate(dats_json, json_schema, format_checker=jsonschema.FormatChecker())
-        logger.info('DATS file is valid. Validation passed.')
+        jsonschema.validate(json_obj, json_schema, format_checker=jsonschema.FormatChecker())
+        logger.info('The file is valid. Validation passed.')
         return True
     except jsonschema.exceptions.ValidationError:
-        errors = [e for e in v.iter_errors((dats_json))]
-        logger.info(f"DATS file is not valid. Total errors: {len(errors)}")
+        errors = [e for e in v.iter_errors((json_obj))]
+        logger.info(f"The file is not valid. Total errors: {len(errors)}")
         for i, error in enumerate(errors, 1):
             logger.error(f"{i} Validation error in {'.'.join(str(v) for v in error.path)}: {error.message}")
         logger.info('Validation failed.')
@@ -50,7 +50,7 @@ def validate_dats(dats_json):
 
 
 def help():
-    return logger.info('Usage: python validator.py --json=doc.json')
+    return logger.info('Usage: python validator.py --file=doc.json')
 
 
 if __name__ == "__main__":
