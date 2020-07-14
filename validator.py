96e4e2385241d96d6a0cc92e355deac97880cc20
ksenia1zaytseva <ksenia1zaytseva@gmail.com>
2020-07-14 15:48:37 -0400

    add non schema validation function

--- validator.py
+++ validator.py
@@ -37,7 +37,7 @@ def main(argv):
     with open(json_filename) as json_file:
         json_obj = json.load(json_file)
         validate_json(json_obj)
-        validate_recursively(json_obj)
+        validate_non_schema_required(json_obj)
 
 
 def validate_json(json_obj):
@@ -54,7 +54,7 @@ def validate_json(json_obj):
         return True
     except jsonschema.exceptions.ValidationError:
         errors = [e for e in v.iter_errors((json_obj))]
-        logger.info(f"The file is not valid. Total errors: {len(errors)}")
+        logger.info(f"The file is not valid. Total json schema errors: {len(errors)}")
         for i, error in enumerate(errors, 1):
             logger.error(f"{i} Validation error in {'.'.join(str(v) for v in error.path)}: {error.message}")
         logger.info('JSON schema validation failed.')
@@ -71,29 +71,23 @@ def validate_extra_properties(dataset):
         # first checks if required extraProperties categories are present
         for category in REQUIRED_EXTRA_PROPERTIES:
             if category not in extra_prop_categories:
-                error_message = f"Validation error: " \
+                error_message = f"Validation error in {dataset['title']}: " \
                                 f"extraProperties - category - {category} is not found."
                 errors.append(error_message)
-                #logger.error(error_message)
 
         # checks if values of required extraProperties are correct according to a controlled vocabulary
         if "CONP_status" in extra_prop_categories:
             for each_value in extra_prop_categories["CONP_status"]:
                 if each_value not in REQUIRED_EXTRA_PROPERTIES["CONP_status"]:
-                    error_message = f"Validation error: extraProperties - category - " \
+                    error_message = f"Validation error in {dataset['title']}: extraProperties - category - " \
                                     f"CONP_status - {each_value} is not allowed value for CONP_status. " \
                                     f"Allowed values are {REQUIRED_EXTRA_PROPERTIES['CONP_status']}."
                     errors.append(error_message)
-                    #logger.error(error_message)
 
         if errors:
-            logger.info(f"Total errors for dataset {dataset['title']}: {len(errors)}.")
-            for i, er in enumerate(errors, 1):
-                logger.error(f"{i} {er}")
-            return False
+            return False, errors
         else:
-            #logger.info(f"Required extra properties validation for dataset {dataset['title']} has passed.")
-            return True
+            return True, errors
 
     # extraProperties is only required property which is not required on dataset_schema level,
     # if it's not present an Exception is raised
@@ -103,13 +97,28 @@ def validate_extra_properties(dataset):
                         f"{[k for k in REQUIRED_EXTRA_PROPERTIES.keys()]}")
 
 
-def validate_recursively(obj):
+def validate_recursively(obj, errors):
     """ Checks all datasets recursively for required extraProperties. """
 
-    validate_extra_properties(obj)
+    val, errors_list = validate_extra_properties(obj)
+    errors.extend(errors_list)
     if "hasPart" in obj:
-        for child_dataset in obj["hasPart"]:
-            validate_recursively(child_dataset)
+        for each in obj["hasPart"]:
+            validate_recursively(each, errors)
+
+
+def validate_non_schema_required(json_obj):
+    """ Checks if json object has all required extra properties beyond json schema. Prints error report. """
+
+    errors = []
+    validate_recursively(json_obj, errors)
+    if len(errors) > 0:
+        logger.error(f"Total required extra properties errors: {len(errors)}")
+        for i, er in enumerate(errors, 1):
+            logger.error(f"{i} {er}")
+        return False
+    else:
+        return True
 
 
 def help():
